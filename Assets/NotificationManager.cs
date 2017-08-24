f25121d727d3c01b027d73a62ce43fb3885c63e1
doggagain <screedo1997@gmail.com>
2017-08-25 02:17:05 +0300

    pee!

--- Assets/NotificationManager.cs
+++ Assets/NotificationManager.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Timers;
+using System;
+using UnityEngine.UI;
 
 public class NotificationManager : MonoBehaviour {
-
+	private Image notification;
 	// Use this for initialization
 	void Start () {
-		gameObject.SetActive (false);
+	/*	GameObject g = GameObject.Find ("Canvas");
+		notification = g.GetComponent<Image> ();
+		Debug.Log (notification);*/
+	//	notification.enabled = false;
+	//	var a = DateTime.Now;
+	//	gameObject.SetActive (false);
+
+		//bla.GetComponent<Image> ().text = robot.GetComponent<Robot> ().hunger.ToString();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () {	 
+		/*DateTime b =  DateTime.Now;
+		Debug.Log (a.Millisecond);
+		if (a.Millisecond - b.Millisecond > 2000) {
+			Debug.Log (a + "sdf");
+			a = b;
+		}*/
+
+			
+	}
+
+
+	 void TimerCallback(object sender, ElapsedEventArgs e){
+		Debug.Log ("aaa");
+		//gameObject.SetActive (true);	
+		var sprite_renderer = GetComponent<SpriteRenderer> ();
+		sprite_renderer.enabled = false;
 	}
 }
---
+++ Assets/Pee.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pee : MonoBehaviour {
+
+	private int peeIndex = 2;
+	private int wait = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			if (Input.GetTouch (0).deltaPosition.x > 0) {
+			//	Debug.Log ("user is swiping to the right");
+				changepic();
+			}
+ 
+			if (Input.GetTouch (0).deltaPosition.x < 0) {
+			//	Debug.Log ("user is swiping to the left");
+				changepic();
+			}
+ 
+			if (Input.GetTouch (0).deltaPosition.y < 0) {
+			//	Debug.Log ("user is swiping to down");
+				changepic();
+			}
+ 
+			if (Input.GetTouch (0).deltaPosition.y > 0) {
+			//	Debug.Log ("user is swiping to up");
+				changepic();
+			}
+		}
+
+	}
+
+	void changepic(){
+		if (peeIndex == 5) {
+			SceneManager.LoadScene("Game");
+		} 
+		else 
+		{
+			Debug.Log ("pee" + peeIndex);
+			if (wait != 8) {
+				wait++;
+			} else {
+				var pee = Resources.Load<Sprite> ("pee" + peeIndex);
+				this.GetComponent<SpriteRenderer> ().sprite = pee;
+				peeIndex++;
+				wait = 0;
+			}
+		}
+	}
+}
