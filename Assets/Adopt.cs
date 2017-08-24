3d64da361b9d4b00f1043919b353c7361f1e5ca0
Gary David <gdavid@energyspectrum.local>
2017-08-24 19:44:34 -0400

    finishing adopting stuff

---
+++ Assets/Adopt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Adopt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0)
+		{
+			Vector2 fingerPos = Input.GetTouch(0).position;
+]	
+			if (fingerPos.x > 275 && fingerPos.y < -285)
+			{
+				Debug.Log(fingerPos.x);
+				SceneManager.LoadScene("Game");
+			}
+		}
+	}
+}
