9fd0375adc13985ed6f38c0e722eb17bd95af306
Gary David <gdavid@energyspectrum.local>
2017-08-24 19:49:14 -0400

    More things with adopt

---
+++ Assets/ChooseThis.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseThis : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0)
+		{
+			Vector2 fingerPos = Input.GetTouch(0).position;
+
+			if (fingerPos.x > 150 && fingerPos.x < 400 && fingerPos.y < -200 && fingerPos.y > -400)
+			{
+				Debug.Log(fingerPos.x);
+				SceneManager.LoadScene("Picked Dog");
+			}
+		}
+	}
+}
