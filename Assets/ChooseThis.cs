c29b0d1de46ccbce1c57cd2ff2c13391451d6293
doggagain <screedo1997@gmail.com>
2017-08-25 03:03:44 +0300

    asdf

--- Assets/ChooseThis.cs
+++ Assets/ChooseThis.cs
@@ -15,8 +15,9 @@ public class ChooseThis : MonoBehaviour {
 		if (Input.touchCount > 0)
 		{
 			Vector2 fingerPos = Input.GetTouch(0).position;
-
-			if (fingerPos.x > 150 && fingerPos.x < 400 && fingerPos.y < -200 && fingerPos.y > -400)
+			Debug.Log (fingerPos.x);
+			Debug.Log (fingerPos.y);
+			if (fingerPos.x < 300 && fingerPos.y < 300)
 			{
 				Debug.Log(fingerPos.x);
 				SceneManager.LoadScene("Picked Dog");
